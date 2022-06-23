f871767d277cfa620ba9efb8de3c498451f671e9
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2022-06-24 05:21:59 +0630

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4040;
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "100mb" }));
 
 app.use("/sayin", express.static("public"));
 
